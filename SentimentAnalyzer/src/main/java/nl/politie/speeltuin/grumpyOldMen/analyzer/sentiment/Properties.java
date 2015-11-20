10e70fa76084ee6f811ea537c44d61731280e49b
Eelco Meuter <eelco@jpoint.nl>
2015-11-20 16:29:30 +0100

    Paths.get does not work for internal resources :(

--- SentimentAnalyzer/src/main/java/nl/politie/speeltuin/grumpyOldMen/analyzer/sentiment/Properties.java
+++ SentimentAnalyzer/src/main/java/nl/politie/speeltuin/grumpyOldMen/analyzer/sentiment/Properties.java
@@ -1,10 +1,6 @@
 package nl.politie.speeltuin.grumpyOldMen.analyzer.sentiment;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Properties {
 
@@ -12,10 +8,8 @@ public class Properties {
 
     private Properties() {
         try {
-            Path defaultPropertyPath = Paths
-                    .get(Properties.class.getClassLoader().getResource("application.properties").toURI());
-            properties.load(Files.newBufferedReader(defaultPropertyPath));
-        } catch (IOException | URISyntaxException e) {
+            properties.load(ClassLoader.getSystemResourceAsStream("application.properties"));
+        } catch (IOException e) {
             System.err.println("Failed to read properties");
             System.exit(-1);
         }
--- SentimentAnalyzer/src/main/java/nl/politie/speeltuin/grumpyOldMen/analyzer/sentiment/dictionary/SentimentDictionary.java
+++ SentimentAnalyzer/src/main/java/nl/politie/speeltuin/grumpyOldMen/analyzer/sentiment/dictionary/SentimentDictionary.java
@@ -3,10 +3,8 @@ package nl.politie.speeltuin.grumpyOldMen.analyzer.sentiment.dictionary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -24,21 +22,16 @@ public class SentimentDictionary {
     private static final String LEXICON_FILE = "lexicon.txt";
 
     private SentimentDictionary() {
-        try {
-            Stream<String> lines = Files
-                    .lines(Paths.get(Sentiment.class.getClassLoader().getResource(LEXICON_FILE).toURI()));
-            lines.map(Sentiment::map).forEach(s -> cache.putIfAbsent(s.getWord(), s));
-        } catch (URISyntaxException | IOException e) {
-            log.error(e.getMessage());
-            log.debug(e.getMessage(), e);
-        }
+        Stream<String> lines = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream
+                (LEXICON_FILE))).lines();
+        lines.map(Sentiment:: map).forEach(s -> cache.putIfAbsent(s.getWord(), s));
     }
 
     public static SentimentDictionary getInstance() {
         return SentimentDictionaryHolder.INSTANCE;
     }
 
-    public Optional<Sentiment> getSentiment(String word){
+    public Optional<Sentiment> getSentiment(String word) {
         return Optional.ofNullable(cache.get(word));
     }
 
