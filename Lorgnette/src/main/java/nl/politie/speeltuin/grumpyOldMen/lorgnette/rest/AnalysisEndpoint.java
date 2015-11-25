38d7d5199164632693e4d2052887600f8668ec72
Eelco Meuter <eelco@jpoint.nl>
2015-11-26 00:29:48 +0100

    animated line

--- Lorgnette/src/main/java/nl/politie/speeltuin/grumpyOldMen/lorgnette/rest/AnalysisEndpoint.java
+++ Lorgnette/src/main/java/nl/politie/speeltuin/grumpyOldMen/lorgnette/rest/AnalysisEndpoint.java
@@ -10,8 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
-import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,7 +22,7 @@ public class AnalysisEndpoint {
 
     public static final String SENTIMENT_TIME = "sentiment-time";
 
-    private static final String SENTIMENT_TIME_ALL = SENTIMENT_TIME +"-all";
+    private static final String SENTIMENT_TIME_ALL = SENTIMENT_TIME + "-all";
 
     private final Session session;
 
@@ -36,11 +35,12 @@ public class AnalysisEndpoint {
     @RequestMapping(value = SENTIMENT_TIME, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<SentimentTimestamp> getSentimentOverTime() {
         Statement query = QueryBuilder.select().column("polarityindex").column("timestamp").from("result").where
-                (QueryBuilder.gt("timestamp", Instant.now().minus(Duration.ofHours(2))));
+                (QueryBuilder.gt("timestamp", new Date(System.currentTimeMillis() - (1000 * 60 * 120))));
         return session.execute(query).all().stream()
                       .map(r -> new SentimentTimestamp(r.getDouble("polarityindex"), r.getDate("timestamp")))
                       .collect(Collectors.toList());
     }
+
     @RequestMapping(value = SENTIMENT_TIME_ALL, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<SentimentTimestamp> getAllSentimentOverTime() {
         Statement query = QueryBuilder.select().column("polarityindex").column("timestamp").from("result");
@@ -48,5 +48,16 @@ public class AnalysisEndpoint {
                       .map(r -> new SentimentTimestamp(r.getDouble("polarityindex"), r.getDate("timestamp")))
                       .collect(Collectors.toList());
     }
+
+    @RequestMapping(value = "dummy", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<SentimentTimestamp> getDummy() {
+
+
+        Statement query = QueryBuilder.select().column("polarityindex").column("timestamp").from("result");
+        return session.execute(query).all().stream()
+                      .map(r -> new SentimentTimestamp(r.getDouble("polarityindex"), r.getDate("timestamp")))
+                      .collect(Collectors.toList());
+    }
+
 
 }
--- Lorgnette/src/main/resources/static/scripts/main.js
+++ Lorgnette/src/main/resources/static/scripts/main.js
@@ -1,11 +1,18 @@
 $(function () {
     var that = this;
     this.count = 0;
+    this.frame = 100 / 25;
+
+
+    var sock = new SockJS('/tweets');
+    sock.onmessage = function (e) {
+        that.count++;
+    };
 
     var ctx = $("#chart").get(0).getContext("2d");
 
     var data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: [],
         datasets: [
             {
                 label: "Polarity index",
@@ -15,20 +22,28 @@ $(function () {
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
+                data: []
             }
         ]
     };
-    var lineChart = new Chart(ctx).Line(data);
+    var lineChart = new Chart(ctx).Line(data,{animationSteps: 10});
+    loadData();
 
-    var sock = new SockJS('/tweets');
-    sock.onmessage = function (e) {
-        that.count++;
-    };
+    function loadData() {
+        var epoch = new Date();
+        var label = epoch.getHours()+":"+epoch.getMinutes()+":00";
+        lineChart.addData([Math.random() * 100],label);
+        if(lineChart.datasets[0].points.length >40){
+            lineChart.removeData();
+            lineChart.l
+        }
+
+    }
 
     window.setInterval(function () {
+        loadData();
         $("#counter").text(that.count);
-    }, 1000 / 25);
+    }, this.frame);
 
 
 });
