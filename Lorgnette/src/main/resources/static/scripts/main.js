710cb1ed93c19bef8a62e5689fcb8a14edfa9144
Eelco Meuter <eelco@jpoint.nl>
2015-11-26 11:05:44 +0100

    typo.... again

--- Lorgnette/src/main/resources/static/scripts/main.js
+++ Lorgnette/src/main/resources/static/scripts/main.js
@@ -31,7 +31,7 @@ $(function () {
     var lineChart = new Chart(ctx, {"animationSteps": 5}).Line(data);
 
     function loadData() {
-        $.getJSON('/analysis/dummy', function (data) {
+        $.getJSON('/analysis/sentiment-time', function (data) {
             $.each(data, function () {
                 var date = new Date(this.timestamp);
                 var label = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
